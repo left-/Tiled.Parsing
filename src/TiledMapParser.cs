a6e028f09ce7f911c91bc86f6768048d4c16c995
left <left@overttime.com>
2023-04-22 00:25:30 +0200

    chore: fix files' license

--- src/TiledMapParser.cs
+++ src/TiledMapParser.cs
@@ -1,3 +1,4 @@
+//
 // Copyright (c) 2023 Luiz Tenorio (https://github.com/left-)
 // Copyright (c) 2012-2021 Scott Chacon and others
 //
@@ -19,6 +20,7 @@
 // LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 
 using System;
 using System.Collections.Generic;
